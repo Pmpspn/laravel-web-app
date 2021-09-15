6b22eeae389da59dfced5942453e08f30fc3f284
Pmpspn <74927851+Pmpspn@users.noreply.github.com>
2021-09-15 15:23:11 +0200

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <div class="flex mt-48">
-        <div class="ml-32 sm:mb-0 font-semibold">
-            <p class="text-gray-100 text-5xl">Salutation brave personne !</p>
-            <p class="text-gray-300 text-3xl mt-4">Sais-tu que nous sommes <a
-                    class="cursor font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-indigo-600">magnifique</a>
-                ?</p>
-        </div>
 
-        <div class="justify-center place-self-center flex w-1/4 ml-32">
-            <img class="animate-ping"alt="gui" width="30%" src="{{secure_asset('img/viasges/gui-removebg-preview.png')}}">
-            <img class="anima-p" alt="ron" width="30%" src="{{secure_asset('img/viasges/ron-removebg-preview.png')}}">
-            <img class="animate-spin" alt="ste" width="30%" src="{{secure_asset('img/viasges/ste-removebg-preview.png')}}">
-            <img class="animate-bounce" alt="ilo" width="30%" src="{{secure_asset('img/viasges/ilo-removebg-preview.png')}}">
-            <img class="anima-b" alt="lud" width="30%" src="{{secure_asset('img/viasges/lud-removebg-preview.png')}}">
-        </div>
-    </div>
+
+        <div class="flex mt-48">
+            <div class="ml-32 sm:mb-0 font-semibold">
+                <p class="text-gray-100 text-5xl">Bienvenue !</p>
+            </div>
 @stop
